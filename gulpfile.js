df815952698e36f00172fa304f01dd0b19295f9c
sal0 <salomemindiashvili5@gmail.com>
2019-07-06 15:50:56 +0400

    added header

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,5 @@ gulp.task('sass', function () {
 });
  
 gulp.task('watch', function () {
-  gulp.watch('scss/style.scss', gulp.parallel('sass'));
+  gulp.watch('scss/*.scss', gulp.parallel('sass'));
 });
